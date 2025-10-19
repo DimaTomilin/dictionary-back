ddf51bd2701e102007cbd29f1b88db765d5ef9e5
Dima Tomilin <tomilin.dimon@gmail.com>
2025-10-19 23:55:15 +0300

    Change work with DB

--- db.ts
+++ db.ts
@@ -1,19 +1,20 @@
-import { Client } from 'pg';
+import { Pool } from 'pg';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const DATABASE_URL = process.env.DATABASE_URL;
 
-export const client = new Client({
+const pool = new Pool({
   connectionString: DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  // ssl: {
+  //   rejectUnauthorized: false,
+  // },
 });
 
+
 const initDb = async () => {
-  await client.connect();
+  await pool.connect();
 
   console.log('Connecting to DB');
 };
@@ -21,12 +22,12 @@ const initDb = async () => {
 initDb();
 
 export async function getWord(word: string) {
-  const words = await client.query(`SELECT * FROM words WHERE word = $1`, [word]);
+  const words = await pool.query(`SELECT * FROM words WHERE word ILIKE $1`, [word]);
   return words;
 }
 
 export async function getWordByPartOfSpeech(word: string, part_of_speech: string) {
-  const words = await client.query(`SELECT * FROM words WHERE word = $1 AND part_of_speech = $2`, [
+  const words = await pool.query(`SELECT * FROM words WHERE word ILIKE $1 AND part_of_speech ILIKE $2`, [
     word,
     part_of_speech,
   ]);
@@ -34,16 +35,16 @@ export async function getWordByPartOfSpeech(word: string, part_of_speech: string
 }
 
 export async function getRandomWord(part_of_speech: string) {
-  const words = await client.query(`SELECT * FROM words WHERE part_of_speech = $1 ORDER BY RANDOM() LIMIT 1;`, [
+  const words = await pool.query(`SELECT * FROM words WHERE part_of_speech ILIKE $1 ORDER BY RANDOM() LIMIT 1;`, [
     part_of_speech,
   ]);
   return words;
 }
 
 export async function getRandomWordWithFirstLetter(part_of_speech: string, letter: string) {
-  const words = await client.query(
-    `SELECT * FROM words WHERE part_of_speech = $1 AND word LIKE '${letter}%' ORDER BY RANDOM() LIMIT 1;`,
-    [part_of_speech],
+  const words = await pool.query(
+    `SELECT * FROM words WHERE part_of_speech ILIKE $1 AND word ILIKE $2 ORDER BY RANDOM() LIMIT 1;`,
+    [part_of_speech, `${letter}%`],
   );
   return words;
 }
